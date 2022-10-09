b82d7e3b14893f7ed6ac4ad8f4bb8685fdb3614e
Javel Wilson <javelawilson@gmail.com>
2022-10-09 16:48:01 -0500

    Implement mobile styling

---
+++ index.js
@@ -0,0 +1,24 @@
+const menuBtn = document.querySelector('.menu')
+const panel = document.querySelector('.filters')
+const panelStyles = window.getComputedStyle(panel)
+
+menuBtn.addEventListener('click', () => {
+  if (panelStyles.getPropertyValue('clip-path') === 'circle(0% at 100% 0%)') {
+    panel.style.clipPath = 'circle(100% at 50% 50%)'
+  } else {
+    panel.style.clipPath = 'circle(0% at 100% 0%)'
+  }
+})
+
+document.addEventListener('mouseup', (event) => {
+  if (panelStyles.getPropertyValue('clip-path') === 'circle(100% at 50% 50%)') {
+    if (!panel.contains(event.target)) {
+      panel.style.clipPath = 'circle(0% at 100% 0%)'
+    }
+  }
+})
+
+const changeHeart = (heart) => {
+  heart.classList.toggle('fas')
+  heart.classList.toggle('far')
+}
